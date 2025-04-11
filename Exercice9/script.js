26b45825048befdea701fdea993e3f08b4acab86
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-04-11 21:55:46 +0200

    Exercice 9

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,12 @@
 // votre code ici
+function checkAge(int) {
+    if (int < 18) {
+        return "Vous êtes mineur.";
+    } else if (int <= 65) {
+        return "Vous êtes majeur.";
+    } else {
+        return "Vous êtes senior.";
+    }
+}
 
 export default checkAge;
