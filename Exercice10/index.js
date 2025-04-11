0e6ddaff87cf51169cd4fef2ca71b41c6f02b795
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-04-11 22:09:56 +0200

    Exercice 10

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,15 @@
 //votre code ici
+function printNumbers(n) {
+    let result;
 
+    for (i = 1; i <= n; i++) {
+
+        result = i + ' ' 
+    }
+
+    return result
+}
 export default printNumbers
+
+
+printNumbers(5); // retournera 1 2 3 4 5
